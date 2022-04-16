92853727ec8cd12d90aec6983dc6644a043572dc
Juan Manuel <68016784+madpingui@users.noreply.github.com>
2022-04-15 23:33:52 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public bool hasEnded = false;
+
+    public TextMeshProUGUI scorePlayer1, scorePlayer2;
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+        scorePlayer1.text = PlayerPrefs.GetInt("Player1").ToString();
+        scorePlayer2.text = PlayerPrefs.GetInt("Player2").ToString();
+        if(PlayerPrefs.GetInt("Player1") == 10 || PlayerPrefs.GetInt("Player2") == 10)
+        {
+            PlayerPrefs.DeleteAll();
+            scorePlayer1.text = "0";
+            scorePlayer2.text = "0";
+        }
+    }
+
+    public void EndGame()
+    {
+        if (hasEnded)
+            return;
+
+        hasEnded = true;
+        StartCoroutine(PlayEndGameAnimation());
+    }
+
+    IEnumerator PlayEndGameAnimation()
+    {
+        //AnimationDeath
+
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public float speed = 3f;
+    public float rotationSpeed = 200f;
+
+    public Joystick joystick;
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.up * speed * Time.fixedDeltaTime, Space.Self);
+        transform.Rotate(Vector3.forward * -joystick.Vertical * rotationSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "killsPlayer")
+        {
+            if (GameManager.Instance.hasEnded == false)
+            {
+                if (transform.parent.name == "Player1")
+                {
+                    PlayerPrefs.SetInt("Player2", PlayerPrefs.GetInt("Player2") + 1);
+                }
+                else if (transform.parent.name == "Player2")
+                {
+                    PlayerPrefs.SetInt("Player1", PlayerPrefs.GetInt("Player1") + 1);
+                }
+            }
+
+            speed = 0;
+            rotationSpeed = 0;
+
+            GameObject.FindObjectOfType<GameManager>().EndGame();  
+        }
+    }
+}
